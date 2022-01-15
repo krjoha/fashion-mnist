74f04d7f73ba878292a7d478629b6ac349089d7b
Kristoffer Johansson <10258920+krjoha@users.noreply.github.com>
2022-01-15 09:56:07 +0000

    Configured 'black' for python code formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='fashion_mnist',
+    name="fashion_mnist",
     packages=find_packages(),
-    version='0.1.0',
-    description='Using the fashion-mnist dataset to demonstrate ML-pipelines and experimentation with DVC.',
-    author='Kristoffer Johansson',
-    license='MIT',
+    version="0.1.0",
+    description="Using the fashion-mnist dataset to demonstrate ML-pipelines and experimentation with DVC.",
+    author="Kristoffer Johansson",
+    license="MIT",
 )
